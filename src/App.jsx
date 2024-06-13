e1bcf7cdf48bf73cb0adaf16f380d4e5c2eedf6f
Vera869 <verea8910@gmail.com>
2024-06-13 15:47:50 +0700

    start sass

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>app-test</h1>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum qui accusamus maxime vitae necessitatibus quasi suscipit! Iusto, eos maiores. Molestiae accusantium maiores officia mollitia in repudiandae laborum dolores aperiam doloremque.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
