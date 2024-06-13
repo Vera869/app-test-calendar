7b271bed6248dbe0b2acb06cd353ee8dea641c2e
Vera869 <verea8910@gmail.com>
2024-06-13 23:23:39 +0700

    + start menu

--- src/components/HeaderBox.jsx
+++ src/components/HeaderBox.jsx
@@ -1,21 +1,21 @@
 import '../styles/header.scss'
 
 export const HeaderBox = () => {
-   return (<>
+   return (<div className='header-box'>
       <div className="header">
          <h1 className="header-head">Добро пожаловать, <span className="header-span"> Михаил!</span>
          </h1>
          <div className="header-img">
-            <img className="img-chat" src='../../public/image/icons/chat icon.svg'/>
+            <img className="img-chat" src='../../public/image/header-icons/chat icon.svg' alt='chat'/>
             <div className="img-user">
-               <img className="img-avtar" src='../../public/image/icons/avatar.svg'/>
-               <img className="img-arrow" src='../../public/image/icons/Arrow.svg'/>
+               <img className="img-avtar" src='../../public/image/header-icons/avatar.svg' alt='avatar'/>
+               <img className="img-arrow" src='../../public/image/header-icons/Arrow.svg' alt='other'/>
             </div>
            
          </div>
-        
+         
       </div>
-       <div className='header-shadow'></div>
-       </>
+      <div className='header-shadow'></div>
+       </div>
    )
 }
--- src/pages/layoutPage.jsx
+++ src/pages/layoutPage.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { HeaderBox } from "../components/HeaderBox";
+import { MenuBox } from "../components/MenuBox";
 
 export const LayoutPage = () => {
   return (
-    <>
+    <div className="conteiner">
+      
       <HeaderBox/>
+      <MenuBox/>
       <Outlet />
       <footer></footer>
-    </>
+    </div>
   );
 };
